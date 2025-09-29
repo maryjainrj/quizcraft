725c6f7e8fc6866b843ce89b2f108252f738aa1d
greeshmaprasad72 <greeshmaprasad72@gmail.com>
2025-09-29 17:21:30 -0400

    backend code first commit

---
+++ backend/controllers/questionController.js
@@ -0,0 +1,3 @@
+exports.uploadFile = async (req, res) => {
+  res.json({ message: 'Placeholder for file upload' });
+};
---
+++ backend/models/question.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const QuestionSchema = new mongoose.Schema({
+  // Placeholder schema, to be expanded
+});
+
+module.exports = mongoose.model('Question', QuestionSchema);
