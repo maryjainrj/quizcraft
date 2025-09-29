614813b0d749128164d54744d39ddc1c3d607f2c
greeshmaprasad72 <greeshmaprasad72@gmail.com>
2025-09-29 17:37:49 -0400

    front end first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Question Generator</h1>
+      <p>Placeholder for file upload form</p>
+    </div>
+  );
+}
+
+export default App;
